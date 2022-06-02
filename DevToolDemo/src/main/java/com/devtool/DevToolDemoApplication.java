71f9519be5ea86091b9ae3e1d1519e5efa09b128
siddheshm67 <47564286+siddheshm67@users.noreply.github.com>
2022-06-02 12:14:51 +0530

    Add files via upload

--- DevToolDemo/src/main/java/com/devtool/DevToolDemoApplication.java
+++ DevToolDemo/src/main/java/com/devtool/DevToolDemoApplication.java
@@ -8,6 +8,7 @@ public class DevToolDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevToolDemoApplication.class, args);
+		System.out.println("second commit");
 	}
 
 }
